88413e1e969194981c19d4bb3f3a578abffc1361
alguive <a.guijarro.vera@gmail.com>
2025-05-02 20:11:39 +0200

    Twig initialization

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,16 +4,26 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MainController
+class MainController extends AbstractController
 {
     #[Route('/')]
     public function homepage(): Response
     {
-        return new Response(
-            '<strong>Starshop</strong>: Your monopoly-busting option for Starship parts!'
-        );
+        $starshipCount = 457;
+        $myShip = [
+            'name' => 'USS LeafyCruisser (NCC-001)',
+            'class' => 'Garden',
+            'captain' => 'Jean-Luck Pickles',
+            'status' => 'Under Construction',
+        ];
+
+        return $this->render('main/homepage.html.twig', [
+            'myShip' => $myShip,
+            'numberOfStarship' => $starshipCount,
+        ]);
     }
 }
