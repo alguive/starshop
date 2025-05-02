52d8e2948121653c0b3a5659943eb7a222f1b655
alguive <a.guijarro.vera@gmail.com>
2025-05-02 22:06:08 +0200

    Starting with services

--- src/Controller/StarshipApiController.php
+++ src/Controller/StarshipApiController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Model\Starship;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -12,8 +13,10 @@ use Symfony\Component\Routing\Attribute\Route;
 class StarshipApiController extends AbstractController
 {
     #[Route('/api/starships')]
-    public function getCollection(): Response
+    public function getCollection(LoggerInterface $logger): Response
     {
+        $logger->info('Starship collection retrieved');
+
         $starShips = [
             new Starship(
                 1,
