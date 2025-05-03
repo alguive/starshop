12206e4909596819c8254e7460b74a8012e8b307
alguive <a.guijarro.vera@gmail.com>
2025-05-03 19:55:39 +0200

    Upgrading API

--- src/Controller/StarshipApiController.php
+++ src/Controller/StarshipApiController.php
@@ -9,14 +9,27 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/api/starships')]
 class StarshipApiController extends AbstractController
 {
-    #[Route('/api/starships')]
+    #[Route('', methods: ['GET'])]
     public function getCollection(StarshipRepository $repository): Response
     {
         $starShips = $repository->findAll();
 
         return $this->json($starShips);
         //        return new Response(json_encode($starShips));
+    }
+
+    #[Route('/{id<\d+>}', methods: ['GET'])]
+    public function get(int $id, StarshipRepository $repository): Response
+    {
+        $starship = $repository->find($id);
+
+        if ($starship === null) {
+            throw $this->createNotFoundException('Starship not found');
+        }
+
+        return $this->json($starship);
     }
 }
